de7257219f467d0f6682c37123735ecd24ca5b31
nickiteks <splint38@gmail.com>
2020-02-25 14:28:12 +0400

    final

---
+++ FurnitureShopBusinessLogic/BindingModels/StorageBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurnitureShopBusinessLogic.BindingModels
+{
+    public class StorageBindingModel
+    {
+        public int Id { get; set; }
+        public string ComponentName { get; set; }
+        public int Count { get; set; }
+    }
+}
---
+++ FurnitureShopView/FormStorage.cs
@@ -0,0 +1,49 @@
+﻿using FurnitureShopBusinessLogic.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Unity;
+
+namespace FurnitureShopView
+{
+    public partial class FormStorage : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        private readonly IComponentLogic logic;
+        public FormStorage(IComponentLogic logic)
+        {
+            InitializeComponent();
+            this.logic = logic;
+        }
+        private void Storage_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        public void LoadData()
+        {
+            try
+            {
+                var list = logic.Read(null);
+                if (list != null)
+                {
+                    dataGridView.DataSource = list;
+                    dataGridView.Columns[0].Visible = false;
+                    dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+
+        }
+    }
+}
