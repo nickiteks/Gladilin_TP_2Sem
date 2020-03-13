533d71c385fc22600fa893a194dec6d9beeabf7a
nickiteks <splint38@gmail.com>
2020-03-13 13:47:27 +0400

    finish

---
+++ FurnitureShopBusinessLogic/Interfaces/IProductLogic.cs
@@ -0,0 +1,14 @@
+﻿using FurnitureShopBusinessLogic.BindingModels;
+using FurnitureShopBusinessLogic.ViewModels;
+using System.Collections.Generic;
+
+
+namespace FurnitureShopBusinessLogic.Interfaces
+{
+    public interface IProductLogic
+    {
+        List<ProductViewModel> Read(ProductBindingModel model);
+        void CreateOrUpdate(ProductBindingModel model);
+        void Delete(ProductBindingModel model);
+    }
+}
