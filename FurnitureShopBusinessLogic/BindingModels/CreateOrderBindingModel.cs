61e2e92fd84edd23a050d2d14afd9a170102082b
nickiteks <splint38@gmail.com>
2020-03-21 15:21:41 +0400

    final

---
+++ FurnitureShopBusinessLogic/BindingModels/CreateOrderBindingModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurnitureShopBusinessLogic.BindingModels
+{
+    public class CreateOrderBindingModel
+    {
+        public int FurnitureId { get; set; }
+        public int Count { get; set; }
+        public decimal Sum { get; set; }
+    }
+}
---
+++ FurnitureShopListImplement/DataListSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurnitureShopListImplement.Models
+{
+    public class DataListSingleton
+    {
+        private static DataListSingleton instance;
+        public List<Component> Components { get; set; }
+        public List<Order> Orders { get; set; }
+        public List<Furniture> Products { get; set; }
+        public List<FurnitureComponent> ProductComponents { get; set; }
+        private DataListSingleton()
+        {
+            Components = new List<Component>();
+            Orders = new List<Order>();
+            Products = new List<Furniture>();
+            ProductComponents = new List<FurnitureComponent>();
+        }
+        public static DataListSingleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new DataListSingleton();
+            }
+            return instance;
+        }
+    }
+}
