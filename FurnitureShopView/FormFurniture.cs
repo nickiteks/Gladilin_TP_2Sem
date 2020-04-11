b20c95353f47fe65048ad8d0dd7ab554e77745b5
nickiteks <splint38@gmail.com>
2020-04-11 22:33:26 +0400

    final

---
+++ FurnitureShopView/FormFurniture.cs
@@ -0,0 +1,182 @@
+﻿using FurnitureShopBusinessLogic.BindingModels;
+using FurnitureShopBusinessLogic.Interfaces;
+using FurnitureShopBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Unity;
+
+
+namespace FurnitureShopView
+{
+    public partial class FormFurniture : Form
+    {
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        public int Id { set { id = value; } }
+        private readonly IFurnitureLogic logic;
+        private int? id;
+        private Dictionary<int, (string, int)> FurnitureComponents;
+
+        public FormFurniture(IFurnitureLogic service)
+        {
+            InitializeComponent();
+            dataGridView.Columns.Add("Id", "Id");
+            dataGridView.Columns.Add("ComponentName", "Компонент");
+            dataGridView.Columns.Add("Count", "Количество");
+            dataGridView.Columns[0].Visible = false;
+            dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.logic = service;
+        }
+        private void FormForniture_Load(object sender, EventArgs e)
+        {
+            if (id.HasValue)
+            {
+                try
+                {
+                    FurnitureViewModel view = logic.Read(new FurnitureBindingModel
+                    {
+                        Id = id.Value
+                    })?[0];
+                    if (view != null)
+                    {
+                        textBoxName.Text = view.FurnitureName;
+                        textBoxPrice.Text = view.Price.ToString();
+                        FurnitureComponents = view.FurnitureComponents;
+                        LoadData();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                FurnitureComponents = new Dictionary<int, (string, int)>();
+            }
+        }
+        private void LoadData()
+        {
+            try
+            {
+                if (FurnitureComponents != null)
+                {
+                    dataGridView.Rows.Clear();
+                    foreach (var pc in FurnitureComponents)
+                    {
+                        dataGridView.Rows.Add(new object[] { pc.Key, pc.Value.Item1, pc.Value.Item2 });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormFurnitureComponent>();
+            if (form.ShowDialog() == DialogResult.OK)
+            {
+                if (FurnitureComponents.ContainsKey(form.Id))
+                {
+                    FurnitureComponents[form.Id] = (form.ComponentName, form.Count);
+                }
+                else
+                {
+                    FurnitureComponents.Add(form.Id, (form.ComponentName, form.Count));
+                }
+                LoadData();
+            }
+        }
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                var form = Container.Resolve<FormFurnitureComponent>();
+                int id = Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value);
+                form.Id = id;
+                form.Count = FurnitureComponents[id].Item2;
+                if (form.ShowDialog() == DialogResult.OK)
+                {
+                    FurnitureComponents[form.Id] = (form.ComponentName, form.Count);
+                    LoadData();
+                }
+            }
+        }
+        private void buttonDel_Click(object sender, EventArgs e)
+        {
+            if (dataGridView.SelectedRows.Count == 1)
+            {
+                if (MessageBox.Show("Удалить запись", "Вопрос", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    try
+                    {
+
+                        FurnitureComponents.Remove(Convert.ToInt32(dataGridView.SelectedRows[0].Cells[0].Value));
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                       MessageBoxIcon.Error);
+                    }
+                    LoadData();
+                }
+            }
+        }
+        private void buttonRef_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Заполните название", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            if (string.IsNullOrEmpty(textBoxPrice.Text))
+            {
+                MessageBox.Show("Заполните цену", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            if (FurnitureComponents == null || FurnitureComponents.Count == 0)
+            {
+                MessageBox.Show("Заполните компоненты", "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+                return;
+            }
+            try
+            {
+                logic.CreateOrUpdate(new FurnitureBindingModel
+                {
+                    Id = id,
+                    FurnitureName = textBoxName.Text,
+                    Price = Convert.ToDecimal(textBoxPrice.Text),
+                    FurnitureComponents = FurnitureComponents
+                });
+                MessageBox.Show("Сохранение прошло успешно", "Сообщение",
+               MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+               MessageBoxIcon.Error);
+            }
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+    }
+}
