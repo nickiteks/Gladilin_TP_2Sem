284f8019a9930f8e5852e0691e60b17a6dbb5c29
nickiteks <55086354+nickiteks@users.noreply.github.com>
2020-06-24 11:11:57 +0400

    Update FurnitureShopDatabase.cs

--- FurnitureShopDatabaseImplement/FurnitureShopDatabase.cs
+++ FurnitureShopDatabaseImplement/FurnitureShopDatabase.cs
@@ -21,5 +21,6 @@ namespace FurnitureShopDatabaseImplement
         public virtual DbSet<FurnitureComponent> FurnitureComponents { set; get; }
         public virtual DbSet<Order> Orders { set; get; }
         public virtual DbSet<Client> Clients { set; get; }
+        public virtual DbSet<Implementer> Implementers { set; get; }
     }
 }
