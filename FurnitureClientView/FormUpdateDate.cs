0d7934b133001912e99de4835622026d25e86fb1
nickiteks <splint38@gmail.com>
2020-06-24 14:13:44 +0400

    fix

---
+++ FurnitureClientView/FormUpdateDate.cs
@@ -0,0 +1,55 @@
+﻿using FurnitureShopBusinessLogic.BindingModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FurnitureClientView
+{
+    public partial class FormUpdateDate : Form
+    {
+        public FormUpdateDate()
+        {
+            InitializeComponent();
+            textBoxClientFIO.Text = Program.Client.ClientFio;
+            textBoxEmail.Text = Program.Client.Login;
+            textBoxPassword.Text = Program.Client.Password;
+        }
+        private void ButtonUpdate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxEmail.Text) &&
+           !string.IsNullOrEmpty(textBoxPassword.Text) &&
+           !string.IsNullOrEmpty(textBoxClientFIO.Text))
+            {
+                try
+                {
+                    APIClient.PostRequest($"api/client/updatedata", new ClientBindingModel
+                    {
+                        Id = Program.Client.Id,
+                        ClientFio = textBoxClientFIO.Text,
+                        Login = textBoxEmail.Text,
+                        Password = textBoxPassword.Text
+                    });
+                    MessageBox.Show("Обновление прошло успешно", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    Program.Client.ClientFio = textBoxClientFIO.Text;
+                    Program.Client.Login = textBoxEmail.Text;
+                    Program.Client.Password = textBoxPassword.Text;
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK,
+                   MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Введите логин, пароль и ФИО", "Ошибка",
+               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
